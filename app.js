c43ad4d0e1d990a7ea1e0f92814a97daa3e90586
tobiashungwe <tobias.hungwe@student.howest.be>
2022-10-14 16:30:19 +0200

    Finish custom service exersise

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+angularApp.config(function ($routeProvider) {
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController',
+    })
+
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController',
+
+    })
+
+
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController',
+
+    })
+        
+
+})
+
+//SERVICES
+angularApp.service('nameService', function() {
+    var self = this;    
+    this.name = "John Doe";
+    this.nameLength = function() {
+        return self.name.length; 
+    }
+});
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$log', '$filter', function ($scope,$log, $filter) {
+angularApp.controller('mainController', ['$scope', '$log', 'nameService',  function ($scope,$log, nameService) {
  
-    
 
+$scope.name = nameService.name; 
+$scope.$watch('name', function() {
+    nameService.name = $scope.name;
+})
+$log.log(nameService.name);
+$log.log(nameService.nameLength());
     
     
+}]);
+
+angularApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function ($scope,$log, $routeParams, nameService) {
+ 
+
+    $scope.num = $routeParams.num || 1; 
+    
+    $scope.name = nameService.name; 
+
+    $scope.$watch('name', function() {
+        nameService.name = $scope.name;
+    })
 }]);
 
 
