60905139dee9eb072141694dbb72d30164828106
tobiashungwe <tobias.hungwe@student.howest.be>
2022-10-14 14:56:42 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', '$log', '$filter', function ($scope,$log, $filter) {
-    $scope.handle = "";
-
-    $scope.lowercasehandle = function() {
-        return $filter('lowercase')($scope.handle);
-    }; 
-
-    $scope.characters = 5;
-    
-
+ 
     
     
 
