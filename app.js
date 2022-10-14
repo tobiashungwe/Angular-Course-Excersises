49c46b740f5fe59f54808019365b06f100427098
tobiashungwe <tobias.hungwe@student.howest.be>
2022-10-14 14:54:53 +0200

    add for double binding exersise not  15

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
- 
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope', '$log', '$filter', function ($scope,$log, $filter) {
+    $scope.handle = "";
+
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    }; 
+
+    $scope.characters = 5;
+    
+
+    
+    
+
+    
+    
+}]);
+
+
+
