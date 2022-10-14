c15619c88421dd88850b55ada417f0d0b3c5a7c7
tobiashungwe <tobias.hungwe@student.howest.be>
2022-10-14 15:35:20 +0200

    finish exersise

--- app.js
+++ app.js
@@ -4,12 +4,45 @@ var angularApp = angular.module('angularApp', []);
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', '$log', '$filter', function ($scope,$log, $filter) {
  
-    
+    $scope.name = "main";
+      
     
 
     
     
 }]);
+//second controller
+angularApp.controller('secondController', ['$scope', '$log', '$filter', function ($scope,$log, $filter) {
+ 
+    $scope.name = "Second";
+     
+    
+
+    
+    
+}]);
+
+window.addEventListener('hashchange', function ()
+{
+    switch (window.location.hash) {
+        case '#/bookmark/1':
+            console.log('Page 1 is cool')
+            break;
+        case '#/bookmark/2':
+            console.log('Page 2 is cool')
+            break;
+        case '#/bookmark/3':
+            console.log('Page 3 is cool')
+            break;
+
+        default:
+            console.log('Strange page, something went wrong')
+            break;
+    }
+
+
+}); 
+
 
 
 
