7ede531d759ca68e57d541fff9ebd83c9cab4798
tobiashungwe <tobias.hungwe@student.howest.be>
2022-10-18 10:34:09 +0200

    Add final bits and finish the course angular js

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-//Module
+// MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-//Routes
-weatherApp.config(function($routeProvider) {
-   $routeProvider
-    .when('/', {
-        templateUrl: 'pages/home.htm',
-        controller: 'homeController'
-    })
-    
-    .when('/forecast', {
-        templateUrl: 'pages/forecast.htm',
-        controller: 'forecastController'
-    })
-    
-});
-
-//SERVICES 
-
-weatherApp.service('cityService', function(){
-    this.city = "New York, NY"; 
-
-})
-
-//CONTROLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
-    $scope.city = cityService.city;
-    $scope.$watch('city', function(){
-        cityService.city = $scope.city;
-    })
-}]);
-
-weatherApp.controller('forecastController', ['$scope', 'cityService',  function($scope, cityService) {
-    $scope.city = cityService.city;
-}]);
